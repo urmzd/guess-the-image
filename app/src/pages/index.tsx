24e5725b96aabba857ca2764803591a5f87aa3b3
Urmzd <urmzds@gmail.com>
2021-05-07 22:49:35 -0300

    [I] Add landing page transitions

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Storage } from "aws-amplify";
 import { navigate } from "gatsby";
-import { Divider, Grid, IconButton } from "@material-ui/core";
+import { Typography, Grid, IconButton } from "@material-ui/core";
 import { CloudUpload, PlayCircleFilled } from "@material-ui/icons";
 import config from "../aws-exports";
 import "../styles/index.css";
@@ -15,93 +15,180 @@ enum Colours {
   Black = "#000",
 }
 
+export enum UIEventStates {
+  DEFAULT = 50,
+  HOVERING = 80,
+  CLICKED = 100,
+}
+
 type IndexPageSubContainerWrapperProps = {
   children: JSX.Element;
-  hovering: boolean;
-  setHovering: (hovering: boolean) => void;
-  onClick: () => void;
+  label: PageLocations;
+  index: SubContainerIndex;
+  eventState: UIEventStates;
+  goTo: () => void;
+  setEventState: (eventState: UIEventStates) => void;
 };
 
 type CustomIconButtonProps = Pick<
   IndexPageSubContainerWrapperProps,
-  "children" | "hovering"
+  "children" | "eventState"
 >;
 
 const IndexPageGridContainerStyles = {
   minHeight: "100vh",
 };
 
-const CustomIconButtonStyles = (hovering?: boolean) => ({
-  color: hovering ? Colours.White : Colours.Black,
+const CustomIconButtonStyles = (eventState?: UIEventStates) => ({
+  color: eventState > UIEventStates.DEFAULT ? Colours.White : Colours.Black,
   fontSize: "3rem",
-  transition: "all ease-in-out 750ms",
+  transition: StyleTransitions.DEFAULT,
 });
 
-const SubContainerStyles = (hovering?: boolean) => ({
-  height: `${hovering === undefined ? 50 : hovering ? 80 : 20}vh`,
-  background: hovering ? Colours.ShopifyGreen : Colours.Transparent,
-  transition: "all ease-in-out 750ms",
+enum StyleTransitions {
+  DEFAULT = "all ease-in-out 750ms",
+}
+
+const SubContainerStyles = (eventState: UIEventStates) => ({
+  height: `${eventState}vh`,
+  background:
+    eventState > UIEventStates.DEFAULT
+      ? Colours.ShopifyGreen
+      : Colours.Transparent,
+  opacity: `${eventState > 0 ? 1 : 0}`,
+  transition: StyleTransitions.DEFAULT,
 });
 
-const CustomIconButton = ({ children, hovering }: CustomIconButtonProps) => (
-  <IconButton style={CustomIconButtonStyles(hovering)}>{children}</IconButton>
+const CustomIconButton = ({ children, eventState }: CustomIconButtonProps) => (
+  <IconButton style={CustomIconButtonStyles(eventState)}>{children}</IconButton>
 );
+const IndexPageSubContainerLabelStyles = (show?: boolean) => ({
+  opacity: +show,
+  transition: StyleTransitions.DEFAULT,
+  color: Colours.White,
+});
 
 const IndexPageSubContainerWrapper = ({
-  hovering,
-  setHovering,
+  eventState,
+  setEventState,
   children,
-  onClick,
-}: IndexPageSubContainerWrapperProps) => (
-  <Grid
-    container
-    item
-    alignItems="center"
-    justify="center"
-    style={SubContainerStyles(hovering)}
-    onMouseLeave={() => setHovering(false)}
-    onMouseEnter={() => setHovering(true)}
-    onClick={onClick}
-  >
-    <CustomIconButton hovering={hovering}>{children}</CustomIconButton>
-  </Grid>
-);
+  label,
+  goTo,
+}: IndexPageSubContainerWrapperProps) => {
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justify="center"
+      alignContent="center"
+      style={SubContainerStyles(eventState)}
+      onMouseEnter={() => setEventState(UIEventStates.HOVERING)}
+      onClick={() =>
+        eventState === UIEventStates.CLICKED
+          ? goTo()
+          : setEventState(UIEventStates.CLICKED)
+      }
+    >
+      <CustomIconButton eventState={eventState}>{children}</CustomIconButton>
+      <Grid
+        item
+        style={IndexPageSubContainerLabelStyles(
+          eventState === UIEventStates.CLICKED
+        )}
+      >
+        <Typography>{`PRESS TO ${label
+          .replace("/", "")
+          .toUpperCase()}`}</Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
 export enum PageLocations {
   PLAY = "/play",
   UPLOAD = "/upload",
 }
 
-export type SubContainerList = ((
-  | IndexPageSubContainerWrapperProps
-  | { divider: true }
-) & { key: `d${number}` | PageLocations })[];
+export type SubContainerList = IndexPageSubContainerWrapperProps[];
+
+export type SubContainerState = {
+  subContainers: [number, number];
+};
+
+export enum SubContainerActionTypes {
+  INCREMENT = "increment",
+}
+
+export type SubContainerAction = {
+  type: SubContainerActionTypes;
+  payload: {
+    subContainerIndex: SubContainerIndex;
+    subContainerEventState: UIEventStates;
+  };
+};
+
+const subContainerInitialState: SubContainerState = {
+  subContainers: [UIEventStates.DEFAULT, UIEventStates.DEFAULT],
+};
+
+type SubContainerIndex = 0 | 1;
+
+const subContainerIncrement = (
+  index: SubContainerIndex,
+  eventState: UIEventStates
+): SubContainerAction => ({
+  type: SubContainerActionTypes.INCREMENT,
+  payload: {
+    subContainerIndex: index,
+    subContainerEventState: eventState,
+  },
+});
+
+const subContainerReducer = (
+  state: SubContainerState,
+  {
+    type,
+    payload: { subContainerIndex, subContainerEventState },
+  }: SubContainerAction
+) => {
+  if (type === SubContainerActionTypes.INCREMENT) {
+    return {
+      subContainers: state.subContainers.map((container, index) =>
+        index === subContainerIndex
+          ? subContainerEventState
+          : UIEventStates.CLICKED - subContainerEventState
+      ),
+    };
+  }
+
+  return state;
+};
 
 const IndexPage = () => {
-  const [hoveringPlayContainer, setHoveringPlayContainer] = useState<boolean>();
-  const [
-    hoveringUploadContainer,
-    setHoveringUploadContainer,
-  ] = useState<boolean>();
+  const [state, dispatch] = useReducer(
+    subContainerReducer,
+    subContainerInitialState
+  );
 
   const containers: SubContainerList = [
     {
-      key: PageLocations.PLAY,
-      hovering: hoveringPlayContainer,
-      setHovering: setHoveringPlayContainer,
+      index: 0,
+      eventState: state.subContainers[0],
+      setEventState: (eventState) =>
+        dispatch(subContainerIncrement(0, eventState)),
       children: <PlayCircleFilled fontSize="inherit" />,
-      onClick: async () => await navigate(PageLocations.PLAY),
+      label: PageLocations.PLAY,
+      goTo: () => navigate(PageLocations.PLAY),
     },
     {
-      key: "d1",
-      divider: true,
-    },
-    {
-      key: PageLocations.UPLOAD,
-      hovering: hoveringUploadContainer,
-      setHovering: setHoveringUploadContainer,
+      index: 1,
+      eventState: state.subContainers[1],
+      setEventState: (eventState) =>
+        dispatch(subContainerIncrement(1, eventState)),
       children: <CloudUpload fontSize="inherit" />,
-      onClick: async () => await navigate(PageLocations.UPLOAD),
+      label: PageLocations.UPLOAD,
+      goTo: () => navigate(PageLocations.UPLOAD),
     },
   ];
 
@@ -113,17 +200,14 @@ const IndexPage = () => {
       justify="space-evenly"
     >
       {containers
-        .map(({ key, ...container }) => ({
-          key,
-          children:
-            "divider" in container ? (
-              <Divider />
-            ) : (
-              <IndexPageSubContainerWrapper {...container} />
-            ),
+        .map(({ index, ...container }) => ({
+          index,
+          children: (
+            <IndexPageSubContainerWrapper {...container} index={index} />
+          ),
         }))
-        .map(({ key, children }) => (
-          <Grid item key={key}>
+        .map(({ index, children }) => (
+          <Grid item key={index}>
             {children}
           </Grid>
         ))}
