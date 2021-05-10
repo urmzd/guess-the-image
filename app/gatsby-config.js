9c7b26959a3a5fd5578d566d03c87c3a0a7803f1
Urmzd <urmzds@gmail.com>
2021-05-10 16:36:34 -0300

    [I] move dir down

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -1,12 +1,11 @@
 module.exports = {
     siteMetadata: {
         title: 'Shopify Image Repository',
-        siteUrl: 'https://urmzd.com',
+        siteUrl: 'https://shopify.urmzd.com',
     },
     flags: {
         FAST_DEV: true,
         DEV_SSR: true,
-        FAST_REFRESH: true,
     },
     plugins: ['gatsby-plugin-sitemap'],
 }
